f3766054ac97d0b4dd804da036cd0ac85c27b640
Markus Rosenfelder <markus.rosenfelder@is.uni-freiburg.de>
2020-01-10 15:19:32 +0900

    extended tutorial

--- README.md
+++ README.md
@@ -67,3 +67,49 @@ python
 <Logger detectron2 (DEBUG)>
 >>> exit()
 ```
+
+To check if everything is working with the models, download this image, save it as `input.jpg` and and the following code:
+`http://images.cocodataset.org/val2017/000000439715.jpg`
+
+```python
+import detectron2
+from detectron2.utils.logger import setup_logger
+
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+import matplotlib.pyplot as plt
+
+im = cv2.imread("./input.jpg")
+plt.imshow(im)
+cfg = get_cfg()
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(
+    "./detectron2/detectron2/model_zoo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+)
+
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+print(outputs["instances"].pred_classes)
+print(outputs["instances"].pred_boxes)
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+plt.imshow(v.get_image()[:, :, ::-1])
+# save image
+plt.savefig("output.jpg")
+```
+The `output.jpg` should look like this:
+![output](output.jpg "Output")
--- bla.py
+++ bla.py
@@ -1,13 +1,12 @@
 import detectron2
 from detectron2.utils.logger import setup_logger
-setup_logger()
 
+setup_logger()
 
 # import some common libraries
 import numpy as np
 import cv2
 import random
-# from google.colab.patches import cv2_imshow
 
 # import some common detectron2 utilities
 from detectron2 import model_zoo
@@ -16,20 +15,17 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 
-
 import matplotlib.pyplot as plt
 
-
-
-
-im = cv2.imread("./input1.jpg")
+im = cv2.imread("./input.jpg")
 plt.imshow(im)
 cfg = get_cfg()
 # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-cfg.merge_from_file("./detectron2/detectron2/model_zoo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.merge_from_file(
+    "./detectron2/detectron2/model_zoo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+)
 
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-# Find a model from detectron2's model zoo. You can either use the https://dl.fbaipublicfiles.... url, or use the detectron2:// shorthand
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
@@ -38,4 +34,5 @@ print(outputs["instances"].pred_boxes)
 v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
 v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 plt.imshow(v.get_image()[:, :, ::-1])
-plt.savefig("test.jpg")
+# save image
+plt.savefig("output.jpg")

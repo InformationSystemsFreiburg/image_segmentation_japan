30655fd990605fc6e43291fdeb691d5cdd9d2d1c
Markus Rosenfelder <markus.rosenfelder@is.uni-freiburg.de>
2020-01-24 14:22:03 +0900

    added window percentage for each output, reworked plotting, added comments

--- process_predictions.py
+++ process_predictions.py
@@ -6,32 +6,44 @@ from pathlib import Path
 import multiprocessing as mp
 from tqdm import tqdm
 from datetime import datetime
+import pandas as pd
 
+
+# define fonts and colors
 font_id = ImageFont.truetype("C:\\Windows\\Fonts\\Arial.ttf", 15)
 font_result = ImageFont.truetype("C:\\Windows\\Fonts\\Arial.ttf", 40)
-text = "50%"
 text_color = (255, 255, 255, 128)
-background_bbox_window = (0, 247, 255, 50)
-background_bbox_building = (255, 167, 14, 50)
+background_bbox_window = (0, 247, 255, 30)
+background_bbox_building = (255, 167, 14, 30)
 background_text = (0, 0, 0, 150)
 background_mask_window = (0, 247, 255, 100)
 background_mask_building = (255, 167, 14, 100)
 device = "cpu"
 
+# this variable is True if you want to plot the output images, False if you only need
+# the CSV
+plot_data = True
+
 def draw_bounding_box(img, bounding_box, text, category, id, draw_box=False):
+    """Draws a bounding box onto the image as well as the building ID and the window 
+    percentage."""
+
     x = bounding_box[0]
     y = bounding_box[3]
-
+    text = str(round(text, 2))
     draw = ImageDraw.Draw(img, "RGBA")
     if draw_box:
         if category == 0:
             draw.rectangle(bounding_box, fill=background_bbox_window, outline=(0, 0, 0))
         elif category == 1:
-            draw.rectangle(bounding_box, fill=background_bbox_building, outline=(0, 0, 0))
+            draw.rectangle(
+                bounding_box, fill=background_bbox_building, outline=(0, 0, 0)
+            )
     w, h = font_id.getsize(id)
 
     draw.rectangle((x, y, x + w, y - h), fill=background_text)
-    draw.text((x , y - h), id, fill=text_color, font=font_id)
+    draw.text((x, y - h), id, fill=text_color, font=font_id)
+    # for buildings, add the window percentage value in the lower right corner
     if category == 1:
         w, h = font_result.getsize(text)
         draw.rectangle((x, y, x + w, y + h), fill=background_text)
@@ -39,6 +51,7 @@ def draw_bounding_box(img, bounding_box, text, category, id, draw_box=False):
 
 
 def draw_mask(img, mask, category):
+    """Draws a mask onto the image."""
 
     img = img.convert("RGBA")
 
@@ -54,34 +67,75 @@ def draw_mask(img, mask, category):
 
     return img
 
-def calculate_window_perc(masks, categories, maybe_filename):
-    # for each building loop for each window
-    # check if and how many Trues of the window lie within the building
-    # count the data in a list
-    # add up all the Trues
-    # count the pixels for each building mask as well
-    # buidling Trues/building_mask -> % of window to fassade
-    pass
-    return window_perc
 
-def create_csv(data):
-    """for each building and window one row with
-    add window id and size as well to csv file
-    save result + building id + image name + mask size in csv file"""
-    pass
+def calculate_window_perc(dataset):
+    """Takes a list of prediction dictionaries as input and calculates the percentage of 
+    window to fassade for each building. The result is save to the dataset. For building
+    data, the actual percentage is saved, for windows, 1.0 is put in."""
 
-def process_data(file_path, plot_data=True):
-    # print(file_path)
+    for i, data in enumerate(dataset):
+        # loop through building
+        if data["category"] == 1:
+            window_areas = []
+
+            building_mask = data["mask"]
+            building_area = np.sum(data["mask"])
+
+            for x in dataset:
+                # for each building, loop through each window
+                if x["category"] == 0:
+                    x["window_percentage"] = 1
+                    pixels_overlapping = np.sum(x["mask"][building_mask])
+
+                    window_areas.append(pixels_overlapping)
+
+            window_percentage = sum(window_areas) / building_area
+
+            data["window_percentage"] = window_percentage
+            data["pixel_area"] = building_area
+    return dataset
+
+
+def create_csv(dataset):
+    """Takes a list of lists of data dictionaries as input, flattens this list, creates
+    a DataFrame, removes unnecessary information and saves it as a CSV."""
+
+    # flatten list
+    dataset = [item for sublist in dataset for item in sublist]
+    df = pd.DataFrame(dataset)
+
+    # calculate the percentage of the building compared to the total image size
+    df["total_image_area"] = df["image_height"] * df["image_width"]
+    df["building_area_perc_of_image"] = df["pixel_area"] / df["total_image_area"]
+    # keep only specific columns
+    df = df[
+        [
+            "file_name",
+            "id",
+            "category",
+            "pixel_area",
+            "building_area_perc_of_image",
+            "window_percentage",
+        ]
+    ]
+    # only keep building information
+    df = df[df["category"] == 1]
+    print(df)
+
+    df.to_csv("./data/predictions/result.csv")
+
+
+def process_data(file_path, plot_data=plot_data):
+    """Takes an prediction result in form of a .pkl-file and draws the mask and bounding
+    box information. From these, the percentage of windows to fassade for each building
+    is calculated and plotted onto the image if plot_data=True."""
     with open(file_path, "rb") as f:
-
+        # the following lines of code extract specific data from the prediction-dict
         prediction = pickle.load(f)
-        # print(dir(prediction["prediction"]["instances"]))
-        # print(prediction["prediction"]["instances"].get_fields()["pred_masks"])
-        # print(prediction["prediction"]["instances"].get_fields()["pred_boxes"])
-        # print(dir(prediction["prediction"]["instances"].get_fields()["pred_boxes"]))
-        # print(prediction["prediction"]["instances"].get_fields()["pred_classes"])
-        # print(prediction["prediction"]["instances"].image_size)
-        # print(prediction["prediction"]["instances"].get_fields()["pred_boxes"].tensor.to(device).numpy())
+
+        image_height = prediction["prediction"]["instances"].image_size[0]
+        image_width = prediction["prediction"]["instances"].image_size[1]
+
         boxes = (
             prediction["prediction"]["instances"]
             .get_fields()["pred_boxes"]
@@ -106,14 +160,20 @@ def process_data(file_path, plot_data=True):
         dataset = []
         counter_window = 0
         counter_building = 0
+        # create a new data-dict as well as IDs for each building and window within an
+        # image
         for i, box in enumerate(boxes):
 
             data = {}
             data["file_name"] = prediction["file_name"]
             data["file_location"] = prediction["file_location"]
+            data["image_height"] = image_height
+            data["image_width"] = image_width
+            # category 0 is always a window
             if categories[i] == 0:
                 data["id"] = f"w_{counter_window}"
                 counter_window = counter_window + 1
+            # category 1 is always a building
             elif categories[i] == 1:
                 data["id"] = f"b_{counter_building}"
                 counter_building = counter_building + 1
@@ -123,15 +183,14 @@ def process_data(file_path, plot_data=True):
             data["mask"] = masks[i]
             dataset.append(data)
 
-        for i, data in enumerate(dataset):
-            calculate_window_perc()
+        dataset = calculate_window_perc(dataset)
 
         if plot_data:
             for i, data in enumerate(dataset):
                 draw_bounding_box(
                     img,
                     data["bounding_box"],
-                    text,
+                    data["window_percentage"],
                     data["category"],
                     data["id"],
                     draw_box=True,
@@ -139,18 +198,19 @@ def process_data(file_path, plot_data=True):
             for i, data in enumerate(dataset):
                 img = draw_mask(img, data["mask"], data["category"])
 
+            img.save(
+                f"./data/prediction_results/{data['file_name']}_prediction.png",
+                quality=95,
+            )
+    return dataset
 
-            img.save(f"./data/prediction_results/{data['file_name']}_prediction.png", quality=95)
 
-
-
-
-def apply_mp_progress(func, n_processes, **kwargs):
+def apply_mp_progress(func, n_processes, prediction_list):
     p = mp.Pool(n_processes)
-    # print(kwargs)
+
     res_list = []
-    with tqdm(total=len(kwargs["file_path"])) as pbar:
-        for i, res in tqdm(enumerate(p.imap_unordered(func, kwargs["file_path"], kwargs["plot_data"]))):
+    with tqdm(total=len(prediction_list)) as pbar:
+        for i, res in tqdm(enumerate(p.imap_unordered(func, prediction_list))):
             pbar.update()
             res_list.append(res)
         pbar.close()
@@ -158,24 +218,18 @@ def apply_mp_progress(func, n_processes, **kwargs):
     p.join()
     return res_list
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     prediction_folder = Path("./data/predictions/")
     prediction_list = []
     start = datetime.now()
     for i, file in enumerate(prediction_folder.glob("*.pkl")):
-        
         file = str(file)
         prediction_list.append(file)
-        # print(file)
-        #file_name = file.split("\\")[-1]
-        #print(file_name)
-        #process_data(file, plot_data=True)
 
-    apply_mp_progress(process_data,  mp.cpu_count(), **{"file_path": prediction_list, "plot_data": True})
-    # process_data(file, plot_data=True)
-    print(datetime.now()- start)
+    dataset = apply_mp_progress(process_data, mp.cpu_count(), prediction_list)
 
+    create_csv(dataset)
 
-
+    print(datetime.now() - start)
 
